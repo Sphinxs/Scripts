584cc314857b1b4e524e252dc291101e384533ed
Walter H <Sphinxs@users.noreply.github.com>
2018-03-14 00:34:34 -0300

    Allegro 5

--- Allegro.sh
+++ Allegro.sh
@@ -8,7 +8,7 @@ sudo su
 
 # Dependencies - Askubuntu.com/questions/840257/e-package-libpng12-0-has-no-installation-candidate-ubuntu-16-10-gnome
 
-apt install -y git build-essential subversion cmake xorg-dev libgl1-mesa-dev libglu-dev install libpng-dev libz-dev libcurl4-gnutls-dev libfreetype6-dev libjpeg-dev libvorbis-dev libopenal-dev libphysfs-dev libgtk2.0-dev libasound-dev libflac-dev libdumb1-dev install exuberant-ctags dvi2ps dvipdfmx latex2html pandoc liballegro-dialog5-dev
+apt install -y git build-essential subversion cmake xorg-dev libgl1-mesa-dev libglu-dev libpng-dev libz-dev libcurl4-gnutls-dev libfreetype6-dev libjpeg-dev libvorbis-dev libopenal-dev libphysfs-dev libgtk2.0-dev libasound-dev libflac-dev libdumb1-dev exuberant-ctags dvi2ps dvipdfmx latex2html pandoc liballegro-dialog5-dev
 
 
 # Ppa
