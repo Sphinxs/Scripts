242f31db9f12ce6ef56fc0e30c52dbe0d5f7453f
Walter <Sphinxs@users.noreply.github.com>
2018-03-18 13:29:30 -0300

    Allegro 5

--- Allegro.sh
+++ Allegro.sh
@@ -21,10 +21,3 @@ add-apt-repository ppa:allegro/5.2
 apt-get update
 
 apt-get install liballegro5-dev
-
-
-# Get some example and put it in a .cpp file : Wiki.allegro.cc/index.php?title=Allegro_5_Tutorial/Displays
-
-# g++ Arch.cpp -o Arch -Wall -I/usr/include/allegro5 -L/usr/local/lib -lallegro && ./Arch
-
-# g++ Arch.cpp -o Arch -I/usr/include/allegro5 -L/usr/local/lib -Wall -lallegro `pkg-config --libs allegro-5 allegro_audio-5 allegro_dialog-5 allegro_image-5 allegro_memfile-5 allegro_primitives-5 allegro_acodec-5 allegro_color-5 allegro_font-5 allegro_main-5 allegro_physfs-5 allegro_ttf-5`
