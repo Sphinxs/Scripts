50b6a044aa9dfb3b508056aa1274cd03b20f4c4b
Walter <Sphinxs@users.noreply.github.com>
2018-03-18 15:43:01 -0300

    Install Mongo

--- Mongo.sh
+++ Mongo.sh
@@ -1,14 +1,8 @@
-
-# chmod +x Mongo.sh
-
-set -e
-
-sudo su
-
+#!/bin/bash
 
 # Public Key
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 
 # Source List - Debian 8
 
