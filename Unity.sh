24c890230a2c90a076b4d631bd49045bbfd9f5b1
Walter <Sphinxs@users.noreply.github.com>
2018-03-18 14:41:14 -0300

    Install Unity 3D
    Script to install Unity 3D at Linux - Ubuntu 17.10

--- Unity.sh
+++ Unity.sh
@@ -1,25 +1,22 @@
 
-# How to install Unity 3D on Linux - Ubuntu 17.10 +
-
-# This script has been tested at Ubuntu Budgie 17.10 using a .deb file, you can also
-# install Unity 3D using a propertly Shell Script or install it using Deepin Os, this
-# distro has the Unity 3D in their Software Store.
-
-# Deepin.org and Download.unity3d.com/download_unity/unity-editor-installer-5.1.0f3+2015082501.sh
-
-
-
-# Permissions
-
-set -e
-
-sudo su
+##########################################################################################################
+#
+# Install Unity 3D on Linux
+#
+# This script has been tested at Ubuntu Budgie 17.10, 64 bits. You can install Unity 3D by using a .deb
+# file manually, like this script, or by using a C# Script.
+#
+# Beta.unity3d.com/download/ee2fb9f9da52/public_download.html
+#
+# If you're a beginner, I recommend that you install Deepin Os and install Unity 3D with just one click
+# by using their Software Manager.
+#
+##########################################################################################################
 
 
-# Update your source list
+# Update source list
 
 apt update
-
 
 
 # Install programs
@@ -32,8 +29,7 @@ apt install git nodejs ffmpeg gzip build-essential default-jre default-jdk gdebi
 apt update && apt install -f
 
 
-
-# Libs
+# Install libs
 
 apt install lib32stdc++6 libasound2 libav-tools libxdamage1 libxdamage-dev lib32gcc1 libc6 libc6-i386 frei0r-plugins libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1 libglib2.0-0 libglu1-mesa libglu1 libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g
 
@@ -43,8 +39,7 @@ apt install lib32stdc++6 libasound2 libav-tools libxdamage1 libxdamage-dev lib32
 apt update && apt install -f && apt dist-upgrade
 
 
-
-# Unity 3D - 2017.2, another releases Forum.unity.com/threads/unity-on-linux-release-notes-and-known-issues.350256
+# Unity 3D - 2017.2, another releases at forum.unity.com/threads/unity-on-linux-release-notes-and-known-issues.350256
 
 wget http://beta.unity3d.com/download/ee86734cf592/unity-editor_amd64-2017.2.0f3.deb
 
@@ -52,7 +47,3 @@ wget http://beta.unity3d.com/download/ee86734cf592/unity-editor_amd64-2017.2.0f3
 # Install Unity 3D
 
 sudo gdebi unity-editor_amd64-2017.2.0f3.deb
-
-
-# Be happy ! ( :
-
