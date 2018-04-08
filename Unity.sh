2afa70f94ecf6ccb80ee11fbd8e989dd209724d1
Walter <Sphinxs@users.noreply.github.com>
2018-04-08 14:46:26 -0300

    Install Unity 3D

--- Unity.sh
+++ Unity.sh
@@ -17,7 +17,7 @@ apt update && apt install -f
 
 # Install libs
 
-apt install lib32stdc++6 libasound2 libav-tools libxdamage1 libxdamage-dev lib32gcc1 libc6 libc6-i386 frei0r-plugins libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1 libglib2.0-0 libglu1-mesa libglu1 libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g
+apt install libcanberra-gtk-module lib32stdc++6 libasound2 libav-tools libxdamage1 libxdamage-dev lib32gcc1 libc6 libc6-i386 frei0r-plugins libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1 libglib2.0-0 libglu1-mesa libglu1 libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g
 
 
 ## Depedencies
