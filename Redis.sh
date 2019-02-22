31c7494396621a9e7e4f15c6b7e0ef0b35acd653
Walter <Sphinxs@users.noreply.github.com>
2019-02-22 15:46:44 -0300

    Redis

--- Redis.sh
+++ Redis.sh
@@ -29,3 +29,9 @@ cp /home/redis/redis-stable/redis.conf /etc/redis
 # Open the _/etc/redis/redis.conf_ file and change `supervised no` to `supervised systemd` and `dir`
 # to `dir /var/lib/redis` or `/usr/local/etc/redis`. If the error _failed to start redis.service,
 # unit redis.service not found_ is issued, [create](https://stackoverflow.com/questions/40317106/failed-to-start-redis-service-unit-redis-server-service-is-masked) the service.
+#
+# https://stackoverflow.com/questions/40317106/failed-to-start-redis-service-unit-redis-server-service-is-masked
+#
+# https://gist.github.com/kapkaev/4619127
+#
+# https://stackoverflow.com/questions/19581059/misconf-redis-is-configured-to-save-rdb-snapshots
