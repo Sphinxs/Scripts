9192f7dec70567c6d5322bd56ea825f61fcc6b12
Walter <Sphinxs@users.noreply.github.com>
2019-02-26 15:38:44 -0300

    Install Popcorn

--- Popcorn.sh
+++ Popcorn.sh
@@ -3,14 +3,14 @@
 # Download
 wget -cq https://mirror02.popcorntime.sh/build/Popcorn-Time-0.3.10-Linux-64.tar.xz
 
-# Give permissions
-sudo su
-
 # Create folder
-mkdir /opt/popcorn
+sudo mkdir /opt/popcorn
 
 # Unzip
-tar -Jxf Popcorn-Time-0.3.10-Linux-64.tar.xz -C /opt/popcorn/
+sudo tar -Jxf Popcorn-Time-0.3.10-Linux-64.tar.xz -C /opt/popcorn/
 
 # Add to menu
-echo '[Desktop Entry]\n Version=0.3\n Encoding=UTF-8\n Name=Popcorn\n Name[en_US]=Popcorn\n GenericName=Torrent Stream Player\n GenericName[en_US]=Torrent Stream Player\n Comment=Run the Popcorn application\n Comment[en_US]=Run the Popcorn application\n Type=Application\n Terminal=false\n Exec=/opt/popcorn/Popcorn-Time\n Icon=/opt/popcorn/src/app/images/icon.png\n Categories=AudioVideo;Player;Video;\n Keywords=Player;Audio;Video;' > /usr/share/applications/popcorn.desktop
+sudo echo '[Desktop Entry]\n Version=0.3\n Encoding=UTF-8\n Name=Popcorn\n Name[en_US]=Popcorn\n GenericName=Torrent Stream Player\n GenericName[en_US]=Torrent Stream Player\n Comment=Run the Popcorn application\n Comment[en_US]=Run the Popcorn application\n Type=Application\n Terminal=false\n Exec=/opt/popcorn/Popcorn-Time\n Icon=/opt/popcorn/src/app/images/icon.png\n Categories=AudioVideo;Player;Video;\n Keywords=Player;Audio;Video;' > /usr/share/applications/popcorn.desktop
+
+# Link
+sudo ln -s /opt/popcorn/Popcorn-Time /usr/bin/popcorn
